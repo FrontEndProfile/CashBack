3a239840da4543a39e3601f82fc282a5a3f5b83a
FrontEndProfile <frontendprofile@gmail.com>
2023-01-08 23:40:43 +0800

    home stage area for QA Testing

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,32 @@
 
 // <!-- Initialize Swiper -->
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
+  slidesPerView: 4,
+  spaceBetween: 8,
   freeMode: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+
+    },
+    580: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 480px
+    991: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 640px
+    1400: {
+      slidesPerView: 4,
+    }
+  }
 });
