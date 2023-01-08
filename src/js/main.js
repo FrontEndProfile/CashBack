f5702d65128dbe844a0ca2ae667d499328d7abb9
FrontEndProfile <frontendprofile@gmail.com>
2023-01-08 14:50:01 +0800

    add slide html fazal v_03

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+
+// <!-- Initialize Swiper -->
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
